280ecb1bbc16cb38c491ea582eaf5eb8bc323e2d
Aaron White <aaron.white46810@gmail.com>
2021-12-18 21:45:35 -0500

    basic ui setup

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("keys");
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+    };
+  };
+
