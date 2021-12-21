1ba7e6eeba64900b0be643f9791afd3e3c3eef8e
DoubleAW <aaron.white46810@gmail.com>
2021-12-21 16:41:15 -0500

    Pre equation text working and all functions working.

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,95 @@
 const keys = document.getElementById("keys");
 const screenText = document.getElementById("screen-text")
-let equation = "";
+const preText = document.getElementById("prescreen-text")
+let evaluated = false
 let prevEquation = ""
 let operator = ""
-let changeText = false
+let changeText = true
 let firstNum = ""
 let secondNum = ""
 let lastClick = ""
 let storedVal = ""
-function digitClicked(btn){
-  if(changeText){
-    changeText = false
-    screenText.textContent = ""
+let lastOp = ""
+
+function valueClicked(btn){
+  
+  if(screenText.textContent.substr(-1) == "." && btn.getAttribute("data-decimal") == "."){
+    console.log("no")
+  }else{
+    
+    if(changeText){
+      if(btn.getAttribute("data-decimal") == "."){
+        screenText.textContent = "0"
+        changeText = false;
+      }else{
+        changeText = false
+        screenText.textContent = ""
+      }
+      
+      
+    }
+    if(evaluated){
+      screenText.textContent = "0"
+      preText.textContent = ""
+      firstNum = ""
+      secondNum = ""
+      evaluated = false
+    }
+    screenText.textContent += btn.textContent
+    storedVal = screenText.textContent
+    
+    lastClick = btn.textContent
   }
   
-  screenText.textContent += btn.textContent
+}
 
+function formatNum(num){
+
+  if(num.substr(-1) == "."){
+    
+    return num.slice(0, -1).toString()
+  }
+  return num.toString()
+}
+
+function backspaceClicked(){
+
+  if(screenText.textContent.length > 0 && screenText.textContent != "0" && !evaluated && !changeText){
+    if(screenText.textContent.length == 1){
+      screenText.textContent = "0"
+      changeText = true
+      storedVal = "0"
+    }else{
+      screenText.textContent = screenText.textContent.slice(0, -1)
+    }
+    
+  }
 }
 
 function operatorClicked(btn){
+  console.log(storedVal)
+  if(operator != ""){
+    lastOp = operator
+  }
   operator = btn.getAttribute("data-operator")
+  if(evaluated){
+    preText.textContent = screenText.textContent + " " + operator
+    storedVal = screenText.textContent
+    evaluated = false
+  }
   if(!changeText){
     if(firstNum == ""){
-      if(screenText.textContent != ""){
-        firstNum = screenText.textContent
+      if(screenText.textContent != "0"){
+        screenText.textContent = formatNum(screenText.textContent)
+        firstNum = formatNum(screenText.textContent)
+        preText.textContent = firstNum + " " + operator + " " 
         changeText = true
       }
       }else{
-        secondNum = screenText.textContent
-        firstNum = evalEquation(operator)
+        secondNum = formatNum(screenText.textContent)
+        
+        firstNum = evalEquation(lastOp)
+        preText.textContent = firstNum + " " + operator
         screenText.textContent = firstNum
         secondNum = ""
         changeText = true
@@ -36,35 +97,38 @@ function operatorClicked(btn){
   }
 }
 function equalClicked(){
-  
-  if(firstNum != "" && !changeText){
-    console.log(firstNum)
+
+  if(firstNum != ""){
     if(screenText.textContent != ""){
-      secondNum = screenText.textContent
-      console.log("first num: " + firstNum + " second num: " + secondNum)
+      secondNum = storedVal
+      
+      console.log(typeof(firstNum) + " " + secondNum)
+      preText.textContent = formatNum(firstNum) + " " + operator + " " + formatNum(secondNum)  + " ="
       firstNum = evalEquation(operator)
       secondNum = ""
+      
       screenText.textContent = firstNum
-      console.log(firstNum)
       changeText = true
+      evaluated = true
     }
   }
 }
 
 function evalEquation(op){
-  if(!changeText){
-    changeText = true;
-    switch(op){
-      case "*":
-        return (parseFloat(firstNum) * parseFloat(secondNum)).toString();
-      case "/":
-        return (parseFloat(firstNum) / parseFloat(secondNum)).toString();
-      case "-":
-        return (parseFloat(firstNum) - parseFloat(secondNum)).toString();
-      case "+":
-        return (parseFloat(firstNum) + parseFloat(secondNum)).toString();
-    }
+  changeText = true;
+  let floatFirst = parseFloat(firstNum)
+  let floatSecond = parseFloat(secondNum)
+  switch(op){
+    case "*":
+      return ((Math.round(((floatFirst * floatSecond) + Number.EPSILON) * 100)) / 100).toString();
+    case "/":
+      return ((Math.round(((floatFirst / floatSecond) + Number.EPSILON) * 100)) / 100).toString();
+    case "-":
+      return ((Math.round(((floatFirst - floatSecond) + Number.EPSILON) * 100) / 100)).toString();
+    case "+":
+      return ((Math.round(((floatFirst + floatSecond) + Number.EPSILON) * 100)) / 100).toString();
   }
+
   
 }
 
@@ -72,5 +136,7 @@ function clearClicked(){
   firstNum = ""
   secondNum = ""
   screenText.textContent = "0"
+  operator = ""
+  preText.textContent = ""
   changeText = true
 }
